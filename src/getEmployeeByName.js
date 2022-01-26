f8dbebba1e4a09300be37e9e0849e6cfe651f7a8
ítalo Lima <contatoitallo@hotmail.com>
2022-01-26 18:27:07 -0300

    Implementa a função getEmployeeByName-Req 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName) {
+    const buscaPessoa = data.employees
+      .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
+    return buscaPessoa;
+  }
+  return {};
 }
 
 module.exports = getEmployeeByName;
