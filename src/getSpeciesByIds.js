a0ca0ac16034093598759a9987b18a45a1d16a96
ítalo Lima <contatoitallo@hotmail.com>
2022-01-26 16:19:17 -0300

    Implementa a função getSpeciesByIds-Req 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return data.species.filter((idAnimal) => ids.includes(idAnimal.id));
 }
 
+// console.log(getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 module.exports = getSpeciesByIds;
