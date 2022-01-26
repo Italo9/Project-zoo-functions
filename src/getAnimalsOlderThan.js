87bb3f671f214f585698705876719098d281b233
ítalo Lima <contatoitallo@hotmail.com>
2022-01-26 18:09:19 -0300

    Implementa a função getAnimalsOlderThan-Req 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
+function getAnimalsOlderThan(animal, age1) {
   // seu código aqui
+  const especie = data.species.find(({ name }) => name === animal);
+  const idadeMinima = especie.residents.every(({ age }) => age > age1);
+  return idadeMinima;
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return data.species.filter((idAnimal) => ids.includes(idAnimal.id));
+  return data.species.filter(({ id }) => ids.includes(id));
 }
 
 // console.log(getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
