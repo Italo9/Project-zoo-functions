a9896f0ada52a19e8c8a5b7dbe7d6ce1c28be001
ítalo Lima <contatoitallo@hotmail.com>
2022-01-28 12:18:11 -0300

    Implementa a função getRelatedEmployees-Req 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const gerente = employees.some(({ managers }) => managers.includes(id));
+  return gerente;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === true) {
+    return employees.filter(({ managers }) => managers.includes(managerId))
+      .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
