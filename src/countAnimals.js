a491394a0c217b401d054616285e914bc60e8206
ítalo Lima <contatoitallo@hotmail.com>
2022-01-29 20:34:24 -0300

    Implementa a função countAnimals- Req 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,16 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal && !animal.sex) {
+    return data.species.find(({ name }) => name === animal.specie).residents.length;
+  } if (animal && animal.sex) {
+    return data.species.find(({ name }) => name === animal.specie)
+      .residents.filter(({ sex }) => sex === animal.sex).length;
+  }
+  return data.species.reduce((acc, { name, residents }) => {
+    acc[name] = residents.length;
+    return acc;
+  }, {});
 }
 
 module.exports = countAnimals;
